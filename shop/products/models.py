7d109fe4399cb0be7cc079d23dd2cb85ab6134f9
Sergey Egorkin <serezhs.e@gmail.com>
2022-09-12 10:11:14 +0300

    добавил модели Категории и Товара

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,3 +1,34 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="products",
+        blank=True,
+        null=True,
+    )
+    price = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    available = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return self.name
