7ced8ee52ee14529bc1e593688a7a0eff8098c26
Sergey Egorkin <serezhs.e@gmail.com>
2022-09-13 11:22:01 +0300

    настроил отображение моделей в админке

--- shop/products/admin.py
+++ shop/products/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category")
+    search_fields = ("name", "price")
+    list_filter = ("available", "category")
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
